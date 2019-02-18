02a3eb187b2b69e2a41b0770398bc5882f419c4f
Matthew Philps <philps.matthew@gmail.com>
2019-02-19 08:25:59 +0900

    test passed

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         const ids = this.state.gifts.map(gift => gift.id);
         const max_id = ids.length > 0 ? Math.max(...ids) : 0
         gifts.push({id: max_id + 1})
-        this.setState = ({gifts})
+        this.setState({gifts})
     }
 
     render () {
@@ -23,6 +23,15 @@ class App extends Component {
                 <h2>
                     Gift Giver
                 </h2>
+                <div className='gift-list'>
+                    {
+                        this.state.gifts.map(gift => {
+                            return (
+                                <div key={gift.id}></div>
+                            )
+                        })
+                    }
+                </div>
                 <Button className='btn-add' onClick={this.addGift}>
                     Add Gift
                 </Button>
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -16,3 +16,8 @@ it('adds a new gift to state when clicking add gift button',()=>{
     app.find('.btn-add').simulate('click')
     expect(app.state().gifts).toEqual([{id: 1}])
 })
+
+it('adds new gift to the rendered list when add gift clicked',()=>{
+    app.find('.btn-add').simulate('click')
+    expect(app.find('.gift-list').children().length).toEqual(2)
+})
