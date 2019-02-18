6205b5ebc7008cd10fe2bd045cfeeeb6e74bb139
Matthew Philps <philps.matthew@gmail.com>
2019-02-19 08:00:14 +0900

    first tests added and passed

---
+++ src/components/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from "./App";
+
+const app = shallow (<App/>);
+
+it('renders correctly',()=>{
+    expect(app).toMatchSnapshot()
+})
+
+it('ititialises the state with empty list of gifts',()=>{
+    expect(app.state().gifts).toEqual([])
+})
+
+it('adds a new gift to state when clicking add gift button',()=>{
+    app.find('.btn-add').simulate('click')
+    expect(app.state().gifts).toEqual([{id: 1}])
+})
